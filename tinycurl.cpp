00b89ffaf753e024fbdb2dc0cacc679b99fb9901
Alexander Sashnov <sashnov@ngs.ru>
2015-04-02 19:23:17 +0600

    features:  * add PUT method  * add HTTP headers support (POST and PUT)  * optional check for HTTP response code

--- tinycurl.cpp
+++ tinycurl.cpp
@@ -4,7 +4,8 @@
  * License: Unlicensed (Public Domain)
  * Homepage: https://github.com/asashnov/tinycurl
  */
- #include "tinycurl.h"
+#include <cstring>
+#include "tinycurl.h"
 
 TinyCurl::TinyCurl(const std::string &url)
 {
@@ -21,10 +22,93 @@ TinyCurl::~TinyCurl()
     curl_easy_cleanup(m_handle);
 }
 
+void TinyCurl::setHTTPHeaders(const headers_t &hdrs) const
+{
+    if (! hdrs.empty())
+    {
+        struct curl_slist *chunk = NULL;
+        for (headers_t::const_iterator i = hdrs.begin() ; i != hdrs.end() ; ++i) {
+            std::string h(i->first);
+            h += ": ";
+            h += i->second;
+            chunk = curl_slist_append(chunk, h.c_str());
+        }
+        CURLcode res = curl_easy_setopt(m_handle, CURLOPT_HTTPHEADER, chunk);
+        if ( res != CURLE_OK)
+            throw Exception(res);
+    }
+}
+
+bool TinyCurl::isHTTPError() const
+{
+    long httpReturnCode;
+    curl_easy_getinfo(m_handle, CURLINFO_RESPONSE_CODE, &httpReturnCode);
+    if (httpReturnCode < 200 || httpReturnCode > 300) {
+       return true;
+    }
+    return false;
+}
+
 std::string TinyCurl::fetch()
 {
     setOptions();
-    sendGetRequest();
+    curlPerform();
+    return m_data;
+}
+
+std::string TinyCurl::post(const std::string &data, const headers_t &hdrs)
+{
+    CURLcode res;
+
+    res = curl_easy_setopt(m_handle, CURLOPT_POST, 1);
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    res = curl_easy_setopt(m_handle, CURLOPT_POSTFIELDS, data.c_str());
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+
+    res = curl_easy_setopt(m_handle, CURLOPT_POSTFIELDSIZE, data.length());
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    setOptions();
+
+    setHTTPHeaders(hdrs);
+
+    curlPerform();
+    return m_data;
+}
+
+
+std::string TinyCurl::put(const std::string &data, const headers_t &hdrs)
+{
+    CURLcode res;
+
+    setOptions();
+
+    res = curl_easy_setopt(m_handle, CURLOPT_UPLOAD, 1);
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    res = curl_easy_setopt(m_handle, CURLOPT_READFUNCTION, tinycurl_read_data_cb);
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    res = curl_easy_setopt(m_handle, CURLOPT_READDATA, this);
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    m_upload_data = data;
+
+    res = curl_easy_setopt(m_handle, CURLOPT_INFILESIZE, static_cast<long>(data.size()));
+    if ( res != CURLE_OK)
+        throw Exception(res);
+
+    setHTTPHeaders(hdrs);
+
+    curlPerform();
     return m_data;
 }
 
@@ -42,6 +126,15 @@ void TinyCurl::setOptions()
     if ( res != CURLE_OK )
         throw Exception(res);
 
+    res = curl_easy_setopt(m_handle, CURLOPT_SSL_VERIFYPEER, 0);
+    if ( res != CURLE_OK )
+        throw Exception(res);
+
+    // TODO: 0 is 'insecure'. Make it a param in TinyCurl
+    res = curl_easy_setopt(m_handle, CURLOPT_SSL_VERIFYHOST, 0);
+    if ( res != CURLE_OK )
+        throw Exception(res);
+
     //set the callback function
     res = curl_easy_setopt(m_handle, CURLOPT_WRITEFUNCTION, tinycurl_write_data_cb);
     if ( res != CURLE_OK )
@@ -53,7 +146,7 @@ void TinyCurl::setOptions()
         throw Exception(res);
 }
 
-void TinyCurl::sendGetRequest()
+void TinyCurl::curlPerform()
 {
     CURLcode res;
     res = curl_easy_perform(m_handle);
@@ -69,8 +162,24 @@ size_t TinyCurl::append_data(void* ptr, size_t size, size_t nmemb)
     return bytes;
 }
 
+size_t TinyCurl::upload_data(void* ptr, size_t size, size_t nmemb)
+{
+    size_t upload_max = size * nmemb;
+    size_t to_send = std::min(m_upload_data.size(), upload_max);
+    memcpy(ptr, m_upload_data.data(), to_send);
+    m_upload_data.erase(0, to_send);
+    return to_send;
+}
+
+
 size_t tinycurl_write_data_cb(void *ptr, size_t size, size_t nmemb, void* pInstance)
 {
     TinyCurl* obj = reinterpret_cast<TinyCurl*>(pInstance);
     return obj->append_data(ptr, size, nmemb);
+}
+
+size_t tinycurl_read_data_cb(void *ptr, size_t size, size_t nmemb, void* pInstance)
+{
+    TinyCurl* obj = reinterpret_cast<TinyCurl*>(pInstance);
+    return obj->upload_data(ptr, size, nmemb);
 }
--- tinycurl.h
+++ tinycurl.h
@@ -11,12 +11,16 @@
  * Homepage: https://github.com/asashnov/tinycurl
  */
 #include <string>
+#include <map>
 #include <stdexcept>
 #include <cstdlib>
 
 #include <curl/curl.h>
 
-extern "C" size_t tinycurl_write_data_cb(void *ptr, size_t size, size_t nmemb, void* pInstance);
+extern "C" {
+  size_t tinycurl_write_data_cb(void *ptr, size_t size, size_t nmemb, void* pInstance);
+  size_t tinycurl_read_data_cb (void *ptr, size_t size, size_t nmemb, void* pInstance);
+}
 
 //! A tiny wrapper around Curl C Library
 class TinyCurl
@@ -33,21 +37,33 @@ public:
     TinyCurl(const std::string &url);
     ~TinyCurl();
 
+    typedef std::map<std::string,std::string> headers_t;
+
     //! Execute GET request
     std::string fetch();
 
+    std::string post(const std::string &data, const headers_t &hdrs);
+
+    std::string put(const std::string &data, const headers_t &hdrs);
+
+    //! true if HTTP error occurs (404 for example)
+    bool isHTTPError() const;
+
     //! append received data into internal buffer by write_data_callback
     size_t append_data(void* ptr, size_t size, size_t nmemb);
+    size_t upload_data(void* ptr, size_t size, size_t nmemb);
 
 private:
     //! no compiler-generated copy constructor
     TinyCurl(const TinyCurl &);
 
     void setOptions();
+    void setHTTPHeaders(const headers_t &hdrs) const;
 
-    void sendGetRequest();
+    void curlPerform();
 
     CURL *m_handle;
     std::string m_url;
     std::string m_data;
+    std::string m_upload_data;
 };
